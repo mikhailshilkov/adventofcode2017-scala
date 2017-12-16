4432fbaae0021d11d2c7f7e74b49a0377fb7fefb
Mikhail Shilkov <mshilkov@astrata.eu>
2017-12-16 01:04:13 +0100

    Day 15 simplified & optimized

--- Day15.scala
+++ Day15.scala
@@ -1,28 +1,20 @@
-def next(v: Long, k: Long) = ((v * k) % 2147483647).toInt
-
-def gen1(seed: Int, multiplier: Int) = 
-  Stream.from(0)
-    .scanLeft(seed)((a, _) => next(a, multiplier))
-
-def gen2(seed: Int, multiplier: Int, predicate: Int => Boolean) = 
-  Stream.from(0)
-    .scanLeft(seed)((a, _) => next(a, multiplier))
+def gen(seed: Long, multiplier: Long, predicate: Long => Boolean) = 
+  Iterator.iterate(seed)(a => (a * multiplier)  % 2147483647)
     .filter(predicate)
 
-
-def generatorsMatch(count: Int, g1: Stream[Int], g2: Stream[Int]) = {
+def generatorsMatch(count: Int, g1: Iterator[Long], g2: Iterator[Long]) = {
   g1.zip(g2)
     .take(count)
-    .filter { case (v1, v2) => (v1 & 65535) == (v2 & 65535) }
+    .filter(v => (v._1 & 65535) == (v._2 & 65535))
     .length
 }
 
 val input = (65, 8921) // Your input
 
-val g1a = gen1(input._1, 16807)
-val g1b = gen1(input._2, 48271)
+val g1a = gen(input._1, 16807, _ => true)
+val g1b = gen(input._2, 48271, _ => true)
 println("d15p1> " + generatorsMatch(40000000, g1a, g1b))
 
-val g2a = gen2(input._1, 16807, _ % 4 == 0)
-val g2b = gen2(input._2, 48271, _ % 8 == 0)
+val g2a = gen(input._1, 16807, _ % 4 == 0)
+val g2b = gen(input._2, 48271, _ % 8 == 0)
 println("d15p2> " + generatorsMatch(5000000, g2a, g2b))
